1ba91038b9cefe29f66f7f23912a8597ef40446a
rcmolina <rcmolina@gmail.com>
2021-05-07 20:30:32 +0200

    Thomson MO5 with tzx direct id15

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ThomsonMO5
+
+Patched conversion utility for better file size and reduced speed.
+
+Follow this procedure:
+
+* k52wav "Bomber (1986) (Hebdogiciel) (PD).k5"
+* sox "Bomber (1986) (Hebdogiciel) (PD).wav" "Bomber (1986) (Hebdogiciel) (PD).voc"
+* direct /t 1464 "Bomber (1986) (Hebdogiciel) (PD).voc" "Bomber (1986) (Hebdogiciel) (PD)_2400Hz.tzx"
---
+++ k52tzx.bat
@@ -0,0 +1,14 @@
+rem k52wav "Bomber (1986) (Hebdogiciel) (PD).k5"
+rem sox "Bomber (1986) (Hebdogiciel) (PD).wav" "Bomber (1986) (Hebdogiciel) (PD).voc"
+rem direct "Bomber (1986) (Hebdogiciel) (PD).voc"
+rem direct /t 1464 "Bomber (1986) (Hebdogiciel) (PD).voc" "Bomber (1986) (Hebdogiciel) (PD)_2400Hz.tzx"
+rem PAUSE
+
+forfiles /m *.k5 /C "cmd /c k52wav @file
+forfiles /m *.wav /C "cmd /c sox @file @fname.voc
+del *.wav
+forfiles /m *.voc /C "cmd /c direct  /t 1464 @file @fname_2400Hz.tzx
+del *.voc
+mkdir K5TZX
+move *.tzx K5TZX
+PAUSE
