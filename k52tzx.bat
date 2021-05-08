f5c41fe774af3de6fcd4eeb36846af21c48aebd3
rcmolina <rcmolina@gmail.com>
2021-05-08 07:39:30 +0200

    Thomson MO5 with tzx direct id15

--- k52tzx.bat
+++ k52tzx.bat
@@ -4,6 +4,7 @@ rem direct "Bomber (1986) (Hebdogiciel) (PD).voc"
 rem direct /t 1464 "Bomber (1986) (Hebdogiciel) (PD).voc" "Bomber (1986) (Hebdogiciel) (PD)_2400Hz.tzx"
 rem PAUSE
 
+ren *.k7 *.k5
 forfiles /m *.k5 /C "cmd /c k52wav @file
 forfiles /m *.wav /C "cmd /c sox @file @fname.voc
 del *.wav
